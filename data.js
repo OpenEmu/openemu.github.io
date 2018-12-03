e571bde60d2333d723c66435214f30bca8f8f6f1
clobber <brymaster@gmail.com>
2018-12-03 11:19:38 -0600

    Add 2.0.8 download links

--- data.js
+++ data.js
@@ -3,12 +3,12 @@ var oe = new Vue({
   el: '.page',
   data: {
     details: {
-      version: '2.0.7',
+      version: '2.0.8',
       req: '10.11'
     },
     download: {
-      latest: 'https://github.com/OpenEmu/OpenEmu/releases/download/v2.0.7/OpenEmu_2.0.7.zip',
-      experimental: 'https://github.com/OpenEmu/OpenEmu/releases/download/v2.0.7/OpenEmu_2.0.7-experimental.zip'
+      latest: 'https://github.com/OpenEmu/OpenEmu/releases/download/v2.0.8/OpenEmu_2.0.8.zip',
+      experimental: 'https://github.com/OpenEmu/OpenEmu/releases/download/v2.0.8/OpenEmu_2.0.8-experimental.zip'
     },
     intro: [
       { th: 'intro-th1.jpg', main: 'intro-nes-grid.png', active: true },
