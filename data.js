bb639fb31440d95cbd82c443f215175de8394207
clobber <brymaster@gmail.com>
2020-12-22 19:47:04 -0700

    Add 2.3 download links

--- data.js
+++ data.js
@@ -3,12 +3,12 @@ var oe = new Vue({
   el: '.page',
   data: {
     details: {
-      version: '2.2.1',
-      req: '10.14'
+      version: '2.3',
+      req: '10.14.4'
     },
     download: {
-      latest: 'https://github.com/OpenEmu/OpenEmu/releases/download/v2.2.1/OpenEmu_2.2.1.zip',
-      experimental: 'https://github.com/OpenEmu/OpenEmu/releases/download/v2.2.1/OpenEmu_2.2.1-experimental.zip'
+      latest: 'https://github.com/OpenEmu/OpenEmu/releases/download/v2.3/OpenEmu_2.3.zip',
+      experimental: 'https://github.com/OpenEmu/OpenEmu/releases/download/v2.3/OpenEmu_2.3-experimental.zip'
     },
     intro: [
       { th: 'intro-th1.jpg', main: 'intro-nes-grid.png', active: true },
